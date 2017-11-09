15c56895aff2f5e93696285f28cabf30e794b393
James Dunne <james.jdunne@gmail.com>
2017-11-09 09:45:46 -0600

    Move high- and low-pass filters into bands.

--- public/eq.js
+++ public/eq.js
@@ -2,53 +2,32 @@
 class EQ {
     constructor(opts) {
         this.opts = opts;
-        this._hpfreq = new Parameter(
-            opts.hpfreq || 0,
-            this.applyHPFreq.bind(this)
-        );
-        this._lpfreq = new Parameter(
-            opts.lpfreq || 22050,
-            this.applyLPFreq.bind(this)
-        );
     }
 
     createNodes(ac) {
-        this.hpNode = ac.createBiquadFilter();
-        this.lpNode = ac.createBiquadFilter();
+        let inputNode = null;
+        let outputNode = null;
+        let bandNodes = [];
 
-        this.hpNode.type = "highpass";
-        this.hpNode.frequency.value = 0;
-        this.lpNode.type = "lowpass";
-        this.lpNode.frequency.value = ac.sampleRate * 0.5;
-
-        this.hpNode.connect(this.lpNode);
-
-        let lastNode = this.lpNode;
         for (let band of this.opts.bands || []) {
             let bandNode = ac.createBiquadFilter();
+            bandNodes.push(bandNode);
+
             bandNode.type = band.type || "peaking";
-            bandNode.frequency.value = band.freq || 1000;
-            bandNode.q = band.q || 1;
+            bandNode.frequency.value = band.freq;
+            bandNode.Q.value = band.q || 0.666667;
             bandNode.gain.value = band.gain || 0;
-            lastNode.connect(bandNode);
-            lastNode = bandNode;
+
+            if (inputNode === null) {
+                inputNode = bandNode;
+            } else {
+                outputNode.connect(bandNode);
+            }
+            outputNode = bandNode;
         }
 
-        this._outputNode = lastNode;
-    }
-
-    get inputNode() { return this.hpNode; }
-    get outputNode() { return this._outputNode; }
-
-    get hpfreq() { return this._hpfreq; }
-    applyHPFreq() {
-        if (!this.hpNode) return;
-        this.hpNode.frequency.value = this._hpfreq.value;
-    }
-
-    get lpfreq() { return this._lpfreq; }
-    applyLPFreq() {
-        if (!this.lpNode) return;
-        this.lpNode.frequency.value = this._lpfreq.value;
+        this.inputNode = inputNode;
+        this.outputNode = outputNode;
+        this.bandNodes = bandNodes;
     }
 }
